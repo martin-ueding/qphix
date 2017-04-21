a350dde2367b2417a3b5b27c8e6fcdff1921497e
Peter Labus <plabus@sissa.it>
2017-04-21 14:30:39 +0200

    Changed the pure twisted-mass interface to match the Wilson interface.
    * this matches the conventions of QPhiX, in particular there is
      no \kappa rescaling
    * the TMDlash class has now two member variables
      derived_mu and
      derived_mu_inv
      which are used in the A * \chi and A^{-1} * \chi multiplications
    * these parameters are not passed anymore in the TMDslash member functions
    * for both A and A^{-1} multiplications two parameters have to be passed
      two the generated kernel routines

--- include/qphix/tm_dslash_def.h
+++ include/qphix/tm_dslash_def.h
@@ -8,187 +8,175 @@
 namespace QPhiX
 {
 
-  /* Specialize - Dslash of float */
-  template<typename FT, int veclen, int soalen, bool compress12>  /* The teplate is the floating point type */
-  class TMDslash {
-  public:
-    // Pack gauges... 
-    typedef typename Geometry<FT,veclen,soalen,compress12>::SU3MatrixBlock  SU3MatrixBlock;
-    typedef typename Geometry<FT,veclen,soalen,compress12>::FourSpinorBlock FourSpinorBlock;
-    typedef typename Geometry<FT,veclen,soalen,compress12>::TwoSpinorBlock TwoSpinorBlock;
-    
+  template<typename FT, int veclen, int soalen, bool compress12>
+    class TMDslash {
 
-    TMDslash(Geometry<FT,veclen, soalen,compress12>* geom_,
-	   double t_boundary_,
-	   double aniso_coeff_S_,
-	   double aniso_coeff_T_);
+      public:
 
-    /* Destructor */
-    ~TMDslash();
-    
-    /* Apply the operator */
-    void tmdslash(FourSpinorBlock* res, 
-		  const FourSpinorBlock* psi, 
-		  const SU3MatrixBlock* u,
-                  double mu,
-                  double mu_inv, 
-		  int isign,
-		  int cb);
-    
-    void tmdslashAChiMinusBDPsi (FourSpinorBlock* res, 
-			         const FourSpinorBlock* psi, 
-			         const FourSpinorBlock* chi,
-			         const SU3MatrixBlock* u,
-//                                 double mu,
-			         double alpha, 
-			         double beta,
-			         int isign,
-			         int cb);
+        typedef typename Geometry<FT,veclen,soalen,compress12>::SU3MatrixBlock  SU3MatrixBlock;
+        typedef typename Geometry<FT,veclen,soalen,compress12>::FourSpinorBlock FourSpinorBlock;
+        typedef typename Geometry<FT,veclen,soalen,compress12>::TwoSpinorBlock  TwoSpinorBlock;
 
+        TMDslash(Geometry<FT, veclen, soalen, compress12>* geom_,
+            double t_boundary_,
+            double aniso_coeff_S_,
+            double aniso_coeff_T_,
+            double Mass_,
+            double TwistedMass_);
+
+        ~TMDslash();
+
+        /* Apply the operator */
+        void dslash(FourSpinorBlock* res,
+            const FourSpinorBlock* psi,
+            const SU3MatrixBlock* u,
+            int isign,
+            int cb);
+
+        void dslashAChiMinusBDPsi(FourSpinorBlock* res,
+            const FourSpinorBlock* psi,
+            const FourSpinorBlock* chi,
+            const SU3MatrixBlock* u,
+            double alpha,
+            double beta,
+            int isign,
+            int cb);
+
+        Geometry<FT, veclen, soalen, compress12>& getGeometry(void) { return (*s); }
 
 
-    Geometry<FT,veclen,soalen,compress12>& getGeometry(void) { return (*s); }
-  private:
-    Geometry<FT,veclen,soalen,compress12>* s;
-    Comms<FT,veclen,soalen,compress12>* comms;
+      private:
 
-    int log2veclen;
-    int log2soalen;
-    void init();
+        Geometry<FT,veclen,soalen,compress12>* s;
+        Comms<FT,veclen,soalen,compress12>* comms;
 
+        int log2veclen;
+        int log2soalen;
+        void init();
 
-    const  int n_threads_per_core;
+        const int n_threads_per_core;
 
-    const int By;
-    const int Bz;
-    const int NCores;
-    const int Sy;
-    const int Sz;
-    const int PadXY;
-    const int PadXYZ;
-    const int MinCt;
+        const int By;
+        const int Bz;
+        const int NCores;
+        const int Sy;
+        const int Sz;
+        const int PadXY;
+        const int PadXYZ;
+        const int MinCt;
 
-    // Antiperiodic Boundary
-    const double t_boundary;
-    bool amIPtMin;
-    bool amIPtMax;
+        // Antiperiodic Boundary
+        const double t_boundary;
+        bool amIPtMin;
+        bool amIPtMax;
 
-    // Anisotropy
-    double aniso_coeff_S;
-    double aniso_coeff_T;
+        // Anisotropy
+        double aniso_coeff_S;
+        double aniso_coeff_T;
 
+        // Wilson & Twisted Mass Parameters
+        double Mass;
+        double TwistedMass;
 
-    Barrier* gBar;
-    Barrier*** barriers;
+        // Derived Twisted Mass Parameters
+        double derived_mu;     // = \mu / \alpha
+        double derived_mu_inv; // = \alpha / ( \mu^2 + \alpha^2 )
 
-    BlockPhase* block_info;
-    int *tmpspc_all; // Space for YZ offsets etc
-    
-    // Masks for comms in X & Y
-    unsigned int xbmask_x0_xodd[2];
-    unsigned int xfmask_xn_xodd[2];
-    unsigned int ybmask_y0;
-    unsigned int yfmask_yn;
-    
-    // Hide Free Constructor 
-    TMDslash();
-    void TMDyzPlus(int tid,
-		 const FourSpinorBlock* psi, 
-		 FourSpinorBlock* res,
-		 const SU3MatrixBlock* u, 
-		 double mu,
-		 double mu_inv,
-		 int cb);
+        Barrier* gBar;
+        Barrier*** barriers;
 
-    void TMDyzMinus(int tid,
-		  const FourSpinorBlock* psi, 
-		  FourSpinorBlock* res,
-		  const SU3MatrixBlock* u,
-		  double mu,
-		  double mu_inv,
- 		  int cb);
+        BlockPhase* block_info;
+        int *tmpspc_all; // Space for YZ offsets etc
 
+        // Masks for comms in X & Y
+        unsigned int xbmask_x0_xodd[2];
+        unsigned int xfmask_xn_xodd[2];
+        unsigned int ybmask_y0;
+        unsigned int yfmask_yn;
 
-    void TMDyzPlusAChiMinusBDPsi(int tid,
-			       const FourSpinorBlock* psi, 
-			       const FourSpinorBlock* chi,
-			       FourSpinorBlock* res,
-			       const SU3MatrixBlock* u, 
-			       double alpha, 
-			       double beta,
-//			       double mu,
-			       int cb);
+        TMDslash(); // Hide Free Constructor
 
+        void TMDyzPlus(int tid,
+            const FourSpinorBlock* psi,
+            FourSpinorBlock* res,
+            const SU3MatrixBlock* u,
+            int cb);
 
-    void TMDyzMinusAChiMinusBDPsi(int tid,
-				const FourSpinorBlock* psi, 
-				const FourSpinorBlock* chi,
-				FourSpinorBlock* res,
-				const SU3MatrixBlock* u, 
-				double alpha, 
-				double  beta,
-//                                double mu,
-				int cb);
+        void TMDyzMinus(int tid,
+            const FourSpinorBlock* psi,
+            FourSpinorBlock* res,
+            const SU3MatrixBlock* u,
+            int cb);
 
+        void TMDyzPlusAChiMinusBDPsi(int tid,
+            const FourSpinorBlock* psi,
+            const FourSpinorBlock* chi,
+            FourSpinorBlock* res,
+            const SU3MatrixBlock* u,
+            double alpha,
+            double beta,
+            int cb);
 
+        void TMDyzMinusAChiMinusBDPsi(int tid,
+            const FourSpinorBlock* psi,
+            const FourSpinorBlock* chi,
+            FourSpinorBlock* res,
+            const SU3MatrixBlock* u,
+            double alpha,
+            double beta,
+            int cb);
 
+        void TMDPsiPlus(const SU3MatrixBlock *u,
+            const FourSpinorBlock *psi_in,
+            FourSpinorBlock *res_out,
+            int cb);
 
-    void TMDPsiPlus(const SU3MatrixBlock *u, const FourSpinorBlock *psi_in, FourSpinorBlock *res_out, double mu, double mu_inv, int cb);
+        void TMDPsiMinus(const SU3MatrixBlock *u,
+            const FourSpinorBlock *psi_in,
+            FourSpinorBlock *res_out,
+            int cb);
 
+        void TMDPsiPlusAChiMinusBDPsi(const SU3MatrixBlock *u,
+            const FourSpinorBlock *psi_in,
+            const FourSpinorBlock *chi,
+            FourSpinorBlock *res_out,
+            double alpha,
+            double beta,
+            int cb);
 
-    void TMDPsiMinus(const SU3MatrixBlock *u, const FourSpinorBlock *psi_in, FourSpinorBlock *res_out, double mu, double mu_inv, int cb);
+        void TMDPsiMinusAChiMinusBDPsi(const SU3MatrixBlock *u,
+            const FourSpinorBlock *psi_in,
+            const FourSpinorBlock *chi,
+            FourSpinorBlock *re_out,
+            double alpha,
+            double beta,
+            int cb);
 
-
-
-    void TMDPsiPlusAChiMinusBDPsi(const SU3MatrixBlock *u, 
-				const FourSpinorBlock *psi_in, 
-				const FourSpinorBlock *chi, 
-				FourSpinorBlock *res_out, 
-				double alpha, double beta,
-//                                double mu,
-				int cb);
-
-
-    void TMDPsiMinusAChiMinusBDPsi(const SU3MatrixBlock *u, 
-				 const FourSpinorBlock *psi_in, 
-				 const FourSpinorBlock *chi, 
-				 FourSpinorBlock *re_out, 
-				 double alpha, double beta, 
-//                                 double mu,
-				 int cb);
-
-
-
-//packTMFaceDir: same as standard wilson.
+        //packTMFaceDir: same as standard wilson.
 #ifdef QPHIX_QMP_COMMS
-     // PACK FACE FOR SENDING
-    void packTMFaceDir(int tid,
-		     const FourSpinorBlock *psi,
-		     FT *res,
-		     int cb, int dir, int fb, int isPlus);
+        // PACK FACE FOR SENDING
+        void packTMFaceDir(int tid,
+            const FourSpinorBlock *psi,
+            FT *res,
+            int cb, int dir, int fb, int isPlus);
 
-    //  RECEIVE AND COMPLETE FACE 
-    void completeTMFaceDir(int tid,
-			 const FT* psi,
-			 FourSpinorBlock* res,
-			 const SU3MatrixBlock* u,
-			 const double beta,
-			 double mu,
-			 double mu_inv,
-			 int cb, int dir, int fb, int isPlus);
+        //  RECEIVE AND COMPLETE FACE
+        void completeTMFaceDir(int tid,
+            const FT* psi,
+            FourSpinorBlock* res,
+            const SU3MatrixBlock* u,
+            const double beta,
+            int cb, int dir, int fb, int isPlus);
 
-    //  RECEIVE AND COMPLETE FACE
-    void completeFaceDirAChiMBDPsi(int tid,
-				   const FT* psi,
-				   FourSpinorBlock* res,
-				   const SU3MatrixBlock* u,
-				   const double beta ,
-				   int cb, int dir, int fb, int isPlus);
+        //  RECEIVE AND COMPLETE FACE
+        void completeFaceDirAChiMBDPsi(int tid,
+            const FT* psi,
+            FourSpinorBlock* res,
+            const SU3MatrixBlock* u,
+            const double beta ,
+            int cb, int dir, int fb, int isPlus);
 #endif
-
-
-
-  }; // Class
+    }; // Class
 
 
 
